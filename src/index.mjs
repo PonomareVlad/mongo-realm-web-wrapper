b351c0a62ba7c696240a7e4661e1e25a04ec8756
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-11-08 06:58:18 +0500

    appIdFromDataUrl

--- src/index.mjs
+++ src/index.mjs
@@ -1,2 +1,3 @@
 export * from "realm-web";
+export * from "./utils.mjs";
 export {MongoRealmClient} from "./client.mjs";
---
+++ src/utils.mjs
@@ -0,0 +1,7 @@
+export function appIdFromDataUrl(dataUrl = "") {
+    const {pathname} = new URL(dataUrl);
+    const [app, id] = pathname.split("/");
+    if (app !== "app" || !id)
+        throw new Error("Invalid Data API URL");
+    return id;
+}
