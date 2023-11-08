8f2b30a75b0ead2b1155a1f0c198b205ab5b8599
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-11-08 07:06:38 +0500

    Update

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,6 +1,6 @@
 export function appIdFromDataUrl(dataUrl = "") {
     const {pathname} = new URL(dataUrl);
-    const [app, id] = pathname.split("/");
+    const [, app, id] = pathname.split("/");
     if (app !== "app" || !id)
         throw new Error("Invalid Data API URL");
     return id;
